c60a05dffb3a8c39f61703fe1709119f9ad14ecc
HONDOH Atsushi <kagyuu@hondou.homedns.org>
2018-10-24 00:41:01 +0900

    adjust for new version of the ghostscript

--- src/main/resources/createBook.sh
+++ src/main/resources/createBook.sh
@@ -25,8 +25,8 @@ do
   A4L_FILE=${A4_FILE}L.pdf
   A4R_FILE=${A4_FILE}R.pdf
   echo "${A3_FILE} => ${A4L_FILE}, ${A4R_FILE}"
-  gs -o $A4L_FILE -sDEVICE=pdfwrite -sPAPERSIZE=a4 -g5950x8420 -c "<</PageOffset[0 0]>> setpagedevice" -f ./a3/$A3_FILE
-  gs -o $A4R_FILE -sDEVICE=pdfwrite -sPAPERSIZE=a4 -g5950x8420 -c "<</PageOffset[-595 0]>> setpagedevice" -f ./a3/$A3_FILE
+  gs -o $A4L_FILE -sDEVICE=pdfwrite -sPAPERSIZE=a4 -g5950x8420 -c "<</PageOffset[0 25]>> setpagedevice" -f ./a3/$A3_FILE
+  gs -o $A4R_FILE -sDEVICE=pdfwrite -sPAPERSIZE=a4 -g5950x8420 -c "<</PageOffset[-525 25]>> setpagedevice" -f ./a3/$A3_FILE
 done
 
 # CONBINE A BLANK PAGE AND A4 DIARY PAGES
